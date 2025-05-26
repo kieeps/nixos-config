406e205a450204ace5407e01dbba3050dd742d64
kieeps <kieeps@kieeps.com>
2025-05-26 19:18:15 +0200

    fixed hostname

--- hosts/Lappen/configuration.nix
+++ hosts/Lappen/configuration.nix
@@ -90,7 +90,7 @@ boot.binfmt.registrations.appimage = {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.theme = true;
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "Lappen"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
